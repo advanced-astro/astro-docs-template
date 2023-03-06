f6c730745f85d5bd5b6c0961dea8f948d54743bf
Rychardt Rothwërkz <redwerkz@gmail.com>
2023-03-06 14:17:24 +0000

    ✨ Add text compression

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,12 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import preact from '@astrojs/preact';
 import react from '@astrojs/react';
+import minifier from 'astro-html-minifier';
 
 export default defineConfig({
 	integrations: [
 		mdx(),
+		minifier(),
 		preact(),
 		react(),
 	],
