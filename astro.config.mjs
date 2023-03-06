8ca46443e30742e4c0c94b3de34b8d6688c020b6
Rychardt Rothwërkz <redwerkz@gmail.com>
2023-03-06 19:12:48 +0000

    🔍️ Add robots.txt and a sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,14 @@
 import { defineConfig } from 'astro/config'
+
 import mdx from '@astrojs/mdx'
 import preact from '@astrojs/preact'
 import react from '@astrojs/react'
+import sitemap from '@astrojs/sitemap'
+
 import minifier from 'astro-html-minifier'
 
 export default defineConfig({
-  integrations: [mdx(), minifier(), preact(), react()],
+  integrations: [mdx(), minifier(), preact(), react(), sitemap()],
   base: `/`,
   site: `https://advanced-astro.dev`
 })
