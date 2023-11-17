4bcfecf8d82cb82d9c24499981c09a8a44e345fb
Rychardt Rothwërkz <redwerkz@gmail.com>
2023-11-17 14:00:27 +0000

    🚀  Bump all deps to lates & astro v3

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,10 +5,8 @@ import preact from '@astrojs/preact'
 import react from '@astrojs/react'
 import sitemap from '@astrojs/sitemap'
 
-import minifier from 'astro-html-minifier'
-
 export default defineConfig({
-  integrations: [mdx(), minifier(), preact(), react(), sitemap()],
+  integrations: [mdx(), preact(), react(), sitemap()],
   base: `/`,
   site: `https://advanced-astro.dev`
 })
